7c79d3fe908a7776a789b13edaf62d5d450bec18
phuongtq <tranphuong.080@gmail.com>
2025-05-12 17:46:08 +0700

    feat(core/vm): support dymamic precompiled gas by `contract` pointer param

--- tests/fuzzers/bls12381/precompile_fuzzer.go
+++ tests/fuzzers/bls12381/precompile_fuzzer.go
@@ -92,7 +92,9 @@ func fuzz(id byte, data []byte) int {
 	}
 	cpy := make([]byte, len(data))
 	copy(cpy, data)
-	_, err := precompile.Run(nil, common.Address{}, common.Address{}, vm.AccountRef(common.Address{}), cpy, nil, false, false)
+	contract := vm.NewPrecompile(vm.AccountRef(common.Address{}), vm.AccountRef(common.BytesToAddress([]byte{id})), common.Big0, gas)
+	contract.Input = cpy
+	_, err := precompile.Run(nil, contract, common.Address{}, vm.AccountRef(common.Address{}), cpy, nil, false, false)
 	if !bytes.Equal(cpy, data) {
 		panic(fmt.Sprintf("input data modified, precompile %d: %x %x", id, data, cpy))
 	}
